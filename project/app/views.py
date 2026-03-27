82ae2a172ccd7d106b6985a346bdf89c16e16212
alaikasadana <alaikasadana@gmail.com>
2026-03-27 10:00:00 +0530

    project work

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render , redirect
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm , UserRegistrationForm
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login
 
 # Create your views here.
 def index(request):
@@ -12,6 +14,8 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-create_at')
     return render(request , 'tweet_list.html', {'tweets':tweets})
 
+
+@login_required
 def tweet_create(request):
     if request.method == "POST":
         form = TweetForm(request.POST , request.FILES)
@@ -27,6 +31,7 @@ def tweet_create(request):
     return render(request , 'tweet_form.html' , {'form' : form})
 
 
+@login_required
 def tweet_edit(request , tweet_id):
     tweet= get_object_or_404(Tweet  , pk=tweet_id , user = request.user)
     if request.method == 'POST':
@@ -43,6 +48,7 @@ def tweet_edit(request , tweet_id):
     return render(request , 'tweet_form.html' , {'form' : form})
 
 
+@login_required
 def tweet_delete(request , tweet_id):
     tweet = get_object_or_404(Tweet , pk=tweet_id , user =request.user)
     if request.method == 'POST':
@@ -51,4 +57,20 @@ def tweet_delete(request , tweet_id):
     
     
     return render(request , 'tweet_confirm_delete.html' , {'tweet' : tweet})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request , user)
+            return redirect('tweet_ist')
+
+    else :
+        form = UserRegistrationForm()
+
+    return render(request , 'registration/register.html','form':form)
     
